88f9b71b2b4ad2f94bb69559da23369a3f0312e2
Daniel-Brum <danielphillipebrum@hotmail.com>
2021-01-27 21:34:26 -0200

    Dash Adjustments
    Now the dash works as a speed boost.

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -8,12 +8,15 @@ public class Player : MonoBehaviour
     private CharacterController characterController;
     private Vector3 playerDirection = Vector3.zero;
     private bool dashReady = true;
+    private bool dashActive = false;
 
     [Header("Movement Properties")]
     [Tooltip("The distance a player can move normally.")]
     [SerializeField] private float movementDistance = 1f;
-    [Tooltip("The distance a player can move when dashing.")]
-    [SerializeField] private float dashDistance = 3f;
+    [Tooltip("The distance multiplier a player gets when dashing.")]
+    [SerializeField] private float dashDistanceMultiplier = 3f;
+    [Tooltip("The dash's duration.")]
+    [SerializeField] private float dashDuration = 1f;
     [Tooltip("The cooldown after using a dash.")]
     [SerializeField] private float dashCooldown = 5f;
     #endregion
@@ -34,24 +37,36 @@ public class Player : MonoBehaviour
     #region Custom Methods
     void MovePlayer() {
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
-        characterController.Move(movement * movementDistance * Time.deltaTime);
+
         if(movement != Vector3.zero) {
             playerDirection = movement.normalized;
         }
+
+        if(dashActive) {
+            movement = movement * dashDistanceMultiplier;
+        }
+
+        characterController.Move(movement * movementDistance * Time.deltaTime);
     }
 
     void Dash() {
         if(dashReady && Convert.ToBoolean(Input.GetAxis("Fire1"))) {
-            characterController.Move(playerDirection * dashDistance * Time.deltaTime);
-            StartCoroutine(WaitForDashCooldown(dashCooldown));
+            StartCoroutine(WaitForDashCooldown(dashDuration, dashCooldown));
         }
     }
     #endregion
 
     #region Coroutines
-    private IEnumerator WaitForDashCooldown(float delay) {
+    private IEnumerator WaitForDashCooldown(float duration, float cooldown) {
         dashReady = false;
-        yield return new WaitForSeconds(delay);
+        dashActive = true;
+
+        yield return new WaitForSeconds(duration);
+
+        dashActive = false;
+
+        yield return new WaitForSeconds(cooldown - duration);
+        
         dashReady = true;
     }
     #endregion
