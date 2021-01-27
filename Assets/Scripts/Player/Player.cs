4408b4b7d85246fd494726c2b1228816a59245e7
Daniel-Brum <danielphillipebrum@hotmail.com>
2021-01-27 21:06:28 -0200

    Player Movement Initial Commit
    Introduces movement with Unity's CharacterController (based on Unity's default input system axes). Implements player movement and dashing (with properly exposed properties).

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    #region Field Declarations
+    private CharacterController characterController;
+    private Vector3 playerDirection = Vector3.zero;
+    private bool dashReady = true;
+
+    [Header("Movement Properties")]
+    [Tooltip("The distance a player can move normally.")]
+    [SerializeField] private float movementDistance = 1f;
+    [Tooltip("The distance a player can move when dashing.")]
+    [SerializeField] private float dashDistance = 3f;
+    [Tooltip("The cooldown after using a dash.")]
+    [SerializeField] private float dashCooldown = 5f;
+    #endregion
+
+    #region Unity Methods
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        MovePlayer();
+        Dash();
+    }
+    #endregion
+
+    #region Custom Methods
+    void MovePlayer() {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        characterController.Move(movement * movementDistance);
+        if(movement != Vector3.zero) {
+            playerDirection = movement.normalized;
+        }
+    }
+
+    void Dash() {
+        if(dashReady && Convert.ToBoolean(Input.GetAxis("Fire1"))) {
+            characterController.Move(playerDirection * dashDistance);
+            StartCoroutine(WaitForDashCooldown(dashCooldown));
+        }
+    }
+    #endregion
+
+    #region Coroutines
+    private IEnumerator WaitForDashCooldown(float delay) {
+        dashReady = false;
+        yield return new WaitForSeconds(delay);
+        dashReady = true;
+    }
+    #endregion
+}
