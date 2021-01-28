ac0bfdadb43b6fde988f3ff0f47b0b0bcc67535d
Daniel-Brum <danielphillipebrum@hotmail.com>
2021-01-27 22:36:55 -0200

    Trap System (initial review)
    Implements base scripts for the trap types and trap activators. Needs review.

---
+++ Assets/Scripts/TrapSystem/TrapActivationController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseTrapActivationController : MonoBehaviour
+{
+    #region Field Declarations
+    [Header("Trap Activation Properties")]
+    [Tooltip("The trap effect to be activated.")]
+    [SerializeField] private BaseTrapEffectController trapEffectController;
+    [Tooltip("The delay before activating a trap.")]
+    [SerializeField] float trapActivationDelay;
+    #endregion
+
+    #region Unity Methods
+    virtual protected void OnTriggerEnter(Collider collider)
+    {
+        if(collider.CompareTag("Player") && trapEffectController.gameObject.activeSelf) {
+            trapEffectController.gameObject.SetActive(true);
+        }
+    }
+    #endregion
+}
---
+++ Assets/Scripts/TrapSystem/TrapEffectController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public abstract class BaseTrapEffectController : MonoBehaviour
+{
+    #region Field Declarations
+    [Header("Trap Effect Properties")]
+    [Tooltip("The trap effect's type (DamageOverTime or InstantDamage).")]
+    [SerializeField] private ETrapEffectType effectType;
+    [Tooltip("The trap effect's duration.")]
+    [SerializeField] private float effectDuration;
+    [Tooltip("The trap effect's number of ticks (only applies to DamageOverTime traps).")]
+    [SerializeField] private int effectTicks;
+    [Tooltip("The trap effect's cooldown.")]
+    [SerializeField] private float effectCooldown;
+    #endregion
+
+    #region Unity Methods
+    virtual protected void OnEnable() {
+        switch(effectType) {
+            case ETrapEffectType.DamageOverTime:
+            ResolveEffect(effectDuration, effectCooldown);
+            break;
+            
+            case ETrapEffectType.InstantDamage:
+            ResolveEffectOverTime(effectDuration, effectTicks, effectCooldown);
+            break;
+        }
+    }
+    #endregion
+
+    #region Custom Methods
+    virtual protected void EnableEffect() {
+
+    }
+
+    virtual protected void TickEffect() {
+
+    }
+
+    virtual protected void DisableEffect() {
+
+    }
+    #endregion
+
+    #region Coroutines
+    private IEnumerator ResolveEffect(float duration, float cooldown) {
+        EnableEffect();
+
+        yield return new WaitForSeconds(duration);
+
+        DisableEffect();
+
+        yield return new WaitForSeconds(cooldown - duration);
+
+        this.gameObject.SetActive(false);
+        //TODO: allow trap to be activated again (though SetActive might be enough)
+    }
+
+    private IEnumerator ResolveEffectOverTime(float duration, int ticks, float cooldown) {
+        WaitForSeconds tickInterval = new WaitForSeconds(duration / (float)ticks);
+
+        for(int i = 0; i < ticks; i++) {
+            TickEffect();
+            yield return tickInterval;
+        }
+
+        DisableEffect();
+
+        yield return new WaitForSeconds(cooldown - duration);
+
+        this.gameObject.SetActive(false);
+        //TODO: allow trap to be activated again (though SetActive might be enough)
+    }
+    #endregion
+}
