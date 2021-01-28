c257efbd7cab1200c40880fd146cb97f68a83844
Paulo H <paulo@ninito.me>
2021-01-28 00:26:43 -0300

    Fix merge conflicts

--- Assets/Scripts/Player/MotorController.cs
+++ Assets/Scripts/Player/MotorController.cs
@@ -2,82 +2,95 @@
 using System.Collections;
 using UnityEngine;
 
-public class MotorController : MonoBehaviour
+namespace RELIC
 {
-    #region Field Declarations
-    private CharacterController characterController;
-    private Vector3 playerDirection = Vector3.zero;
-    private Vector3 dashDirection = Vector3.zero;
-    private bool dashReady = true;
-    private bool dashActive = false;
-
-    [Header("Movement Properties")]
-    [Tooltip("The distance a player can move normally.")]
-    [SerializeField] private float movementDistance = 1f;
-    [Tooltip("The distance multiplier a player gets when dashing.")]
-    [SerializeField] private float dashDistanceMultiplier = 3f;
-    [Tooltip("The dash's duration.")]
-    [SerializeField] private float dashDuration = 1f;
-    [Tooltip("The cooldown after using a dash.")]
-    [SerializeField] private float dashCooldown = 5f;
-    [Tooltip("Should direction control during dashing be enabled?")]
-    [SerializeField] private bool canChangeDirectionDuringDash = false;
-    #endregion
-
-    #region Unity Methods
-    void Start()
+    public class MotorController : MonoBehaviour
     {
-        characterController = GetComponent<CharacterController>();
-    }
+        #region Field Declarations
+        private CharacterController characterController;
+        private Vector3 playerDirection = Vector3.zero;
+        private Vector3 dashDirection = Vector3.zero;
+        private bool dashReady = true;
+        private bool dashActive = false;
 
-    void Update()
-    {
-        MovePlayer();
-        Dash();
-    }
-    #endregion
+        [Header("Movement Properties")]
+        [Tooltip("The distance a player can move normally.")]
+        [SerializeField] private float movementDistance = 1f;
+        [Tooltip("The distance multiplier a player gets when dashing.")]
+        [SerializeField] private float dashDistanceMultiplier = 3f;
+        [Tooltip("The dash's duration.")]
+        [SerializeField] private float dashDuration = 1f;
+        [Tooltip("The cooldown after using a dash.")]
+        [SerializeField] private float dashCooldown = 5f;
+        [Tooltip("Should direction control during dashing be enabled?")]
+        [SerializeField] private bool canChangeDirectionDuringDash = false;
+        #endregion
 
-    #region Custom Methods
-    void MovePlayer() {
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
-
-        if(movement != Vector3.zero) {
-            playerDirection = movement.normalized;
+        #region Unity Methods
+        void Start()
+        {
+            characterController = GetComponent<CharacterController>();
         }
 
-        if(dashActive) {
-            if(canChangeDirectionDuringDash) {
-                movement = movement * dashDistanceMultiplier;
-            } else {
-                movement = dashDirection * dashDistanceMultiplier;
+        void Update()
+        {
+            MovePlayer();
+            Dash();
+        }
+        #endregion
+
+        #region Custom Methods
+        void MovePlayer()
+        {
+            Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+
+            if (movement != Vector3.zero)
+            {
+                playerDirection = movement.normalized;
+            }
+
+            if (dashActive)
+            {
+                if (canChangeDirectionDuringDash)
+                {
+                    movement = movement * dashDistanceMultiplier;
+                }
+                else
+                {
+                    movement = dashDirection * dashDistanceMultiplier;
+                }
+            }
+
+            characterController.Move(movement * movementDistance * Time.deltaTime);
+        }
+
+        void Dash()
+        {
+            if (dashReady && playerDirection != Vector3.zero && Convert.ToBoolean(Input.GetAxis("Fire1")))
+            {
+                StartCoroutine(ResolveDash(dashDuration, dashCooldown));
             }
         }
+        #endregion
 
-        characterController.Move(movement * movementDistance * Time.deltaTime);
-    }
+        #region Coroutines
+        private IEnumerator ResolveDash(float duration, float cooldown)
+        {
+            dashReady = false;
+            dashActive = true;
+            if (!canChangeDirectionDuringDash)
+            {
+                dashDirection = playerDirection;
+            }
 
-    void Dash() {
-        if(dashReady && playerDirection != Vector3.zero && Convert.ToBoolean(Input.GetAxis("Fire1"))) {
-            StartCoroutine(ResolveDash(dashDuration, dashCooldown));
+            yield return new WaitForSeconds(duration);
+
+            dashActive = false;
+
+            yield return new WaitForSeconds(cooldown - duration);
+
+            dashReady = true;
         }
+        #endregion
     }
-    #endregion
-
-    #region Coroutines
-    private IEnumerator ResolveDash(float duration, float cooldown) {
-        dashReady = false;
-        dashActive = true;
-        if(!canChangeDirectionDuringDash) {
-            dashDirection = playerDirection;
-        }
-
-        yield return new WaitForSeconds(duration);
-
-        dashActive = false;
-
-        yield return new WaitForSeconds(cooldown - duration);
-        
-        dashReady = true;
-    }
-    #endregion
 }
