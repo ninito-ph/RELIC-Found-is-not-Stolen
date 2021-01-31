91a18dc34a9987dfe85f7594307a866747bf4070
Daniel-Brum <danielphillipebrum@hotmail.com>
2021-01-30 21:49:47 -0300

    Changed values on SplitscreenController.cs

--- Assets/Game/Scripts/Utility/SplitscreenController.cs
+++ Assets/Game/Scripts/Utility/SplitscreenController.cs
@@ -44,21 +44,24 @@ namespace RELIC
             {
                 case 2:
                     // Vertical double splitscreen coordinates for cameras
-                    gameCameras[0].rect = new Rect(0f, 0f, 0.5f, 1f);
-                    gameCameras[1].rect = new Rect(0.5f, 0f, 0.5f, 1f);
+                    gameCameras[0].rect = new Rect(0.25175f, 0.5117f, 0.482f, 0.465f);
+                    gameCameras[1].rect = new Rect(0.25175f, 0.023f, 0.482f, 0.465f);
+                    // total border size (percentage) = 0.036
+                    // total width border size (pixels) = 0.036 x 1980 = 71.28
+                    // desired total height border size (pixels) = 71.28 / 1020 ~= 0.07
                     break;
                 case 3:
                     // Three-way splitscreen coordinates for cameras
-                    gameCameras[0].rect = new Rect(0f, 0.5f, 0.5f, 0.5f);
-                    gameCameras[1].rect = new Rect(0.5f, 0.5f, 0.5f, 0.5f);
-                    gameCameras[2].rect = new Rect(0f, 0f, 0.5f, 0.5f);
+                    gameCameras[0].rect = new Rect(0.012f, 0.5117f, 0.482f, 0.465f);
+                    gameCameras[1].rect = new Rect(0.506f, 0.5117f, 0.482f, 0.465f);
+                    gameCameras[2].rect = new Rect(0.012f, 0.023f, 0.482f, 0.465f);
                     break;
                 case 4:
                     // Four-way splitscreen coordinates for cameras
-                    gameCameras[0].rect = new Rect(0f, 0.5f, 0.5f, 0.5f);
-                    gameCameras[1].rect = new Rect(0.5f, 0.5f, 0.5f, 0.5f);
-                    gameCameras[2].rect = new Rect(0f, 0f, 0.5f, 0.5f);
-                    gameCameras[3].rect = new Rect(0.5f, 0f, 0.5f, 0.5f);
+                    gameCameras[0].rect = new Rect(0.012f, 0.5117f, 0.482f, 0.465f);
+                    gameCameras[1].rect = new Rect(0.506f, 0.5117f, 0.482f, 0.465f);
+                    gameCameras[2].rect = new Rect(0.012f, 0.023f, 0.482f, 0.465f);
+                    gameCameras[3].rect = new Rect(0.506f, 0.023f, 0.482f, 0.465f);
                     break;
                 default:
                     Debug.LogError("Unsupported camera amount! Something went wrong, because this part of the code is supposed to be unreachable!");
